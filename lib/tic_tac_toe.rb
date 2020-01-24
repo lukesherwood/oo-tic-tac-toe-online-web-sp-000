ff787dfdc0a0fa114becf5673b2f59fae2baf44b
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-24 04:32:27 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,31 @@
+class TicTacToe
+  WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], #left column
+  [1,4,7], # middle column
+  [2,5,8], #right column
+  [0,4,8], #diagonal 1
+  [2,4,6] #diagonal 2
+  ]
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+ 
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+ 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+end
